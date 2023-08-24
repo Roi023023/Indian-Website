730e9e5b2e8167737a655fb082abe94f0343a50c
yonatan62862 <yonatan62862@gmail.com>
2023-08-24 17:51:56 +0300

    removed redundant code

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(express.json());
 
-app.use('/home', require('./routes/home'));
+app.use('/', require('./routes/home'));
 
 const http = require('http').Server(app);
 // app.set("io", someSocketFile);
