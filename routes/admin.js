17450aefc5797ba411b7222c4c20558733ec379d
yonatan62862 <134393514+yonatan62862@users.noreply.github.com>
2023-08-26 19:31:32 +0300

    Revert "newchange"

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const adminController = require('../controllers/admin'); // Import the admin controller
+const adminController = require('../controllers/admin'); // Referencing the 'admin.js' controller file
 
 const router = express.Router();
 
-router.get('/', adminController.getAdminPage); // Route to the admin page
+router.route('/').get(adminController.getAdminPage); // Defining the route for the admin page
 
 module.exports = router;
--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/users'); // Import the user controller
-
-const router = express.Router();
-
-router.get('/', userController.getAllUsers); // Route to display all users
-router.get('/:id/delete', userController.deleteUser); // Route to delete a user
-
-module.exports = router;
